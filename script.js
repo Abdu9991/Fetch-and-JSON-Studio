89d1fb57c542619c33e262a6c82395814b44aad9
Abdu9991 <abduadam688@gmail.com>
2023-07-25 17:47:48 -0400

    chaning JavaScript file and adding html bonus mission

--- script.js
+++ script.js
@@ -1,24 +1,37 @@
 // TODO: add code here
-
 window.addEventListener('load', function(){
-    fetch( "https://handlers.education.launchcode.org/static/astronauts.json").then(function(response  ){
-        return response.json();    //console.log(json);  
-    }).then(function(data){
-        //console.log(data);
-        const getData = document.getElementById("container");
+ const container = document.getElementById("container");
+async function fetchAndDisplayAstronauts(){
+    
+    let response = await fetch('https://handlers.education.launchcode.org/static/astronauts.json');
+        let data = await response.json();    //console.log(json);  
+        //bonus mission 1
+        data.sort(function (a,b){
+            return a.hoursInSpace < b.hoursInSpace ? 1 : -1;
+        });
+      // bonus mission 3 
+      const count = document.getElementById('count');
+      count.innerHTML = ` these ${data.length} extraordinary people are a few of the handful of humankind who 
+      have had the privilege of looking down on our beautful planet from the quiet darkness of outer space`
         for(let i =0; i < data.length; i++){
-            getData.innerHTML += `<div class="astronaut">
+            let astronaut = data[i];
+            // bonus mission 2
+            let activeClass = astronaut.active ? "active" : "";
+
+            container.innerHTML += `<div class="astronaut">
             <div class="bio">
-               <h3> ${data[i].firstName} ${data[i].lastName}</h3>
+               <h3> ${astronaut.firstName} ${astronaut.lastName}</h3>
                <ul>
-                  <li>Hours in space: ${data[i].hoursInSpace}</li>
-                  <li>Active: ${data[i].active}</li>
-                  <li>Skills: ${data[i].skills}</li>
+                  <li>Hours in space: ${astronaut.hoursInSpace}</li>
+                  <li class="${activeClass}">Active: ${astronaut.active}</li>
+                  <li>Skills: ${astronaut.skills.join(", ")}</li>
                </ul>
             </div>
-            <img class="avatar" src=${data[i].picture}>
+            <img class="avatar" src=${astronaut.picture}>
          </div>`
         }
-    });
     
+        
+}
+fetchAndDisplayAstronauts();
 });
